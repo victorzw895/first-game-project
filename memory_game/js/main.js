15b21ed63ffb65ee4f36414865f9a37933c58b5b
Victor Zhang <victorz895@gmail.com>
2019-06-10 22:05:58 +1000

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,9 @@
+console.log("Up and running!");
+
+var cardOne= "queen",
+	cardTwo= "queen",
+	cardThree= "king",
+	cardFour= "king";
+
+console.log("User flipped " + cardTwo);
+console.log("User flipped " + cardFour)

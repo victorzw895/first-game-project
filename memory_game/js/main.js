ba996b8c51d4cdafa16c65afee3a0dac235845b7
Victor Zhang <victorz895@gmail.com>
2019-06-12 20:09:09 +1000

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,43 @@
 console.log("Up and running!");
 
-var cardOne= "queen",
-	cardTwo= "queen",
-	cardThree= "king",
-	cardFour= "king";
+var cards = ["queen", "queen", "king", "king"],
+	cardsInPlay = [],
+	cardOne = cards[0],
+	cardTwo = cards[2];
 
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+console.log("User flipped " + cardOne);
 console.log("User flipped " + cardTwo);
-console.log("User flipped " + cardFour)
+console.log(cardsInPlay);
+
+//CHECK FOR A MATCH PSEUDO CODE
+// after card is selected
+// check if cardsInPlay.length is 2
+// if length is 2, check for a match
+// if match display message
+// if not a match game continues
+function selectCard() {
+	if (cardsInPlay.length === 2) {
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+			alert("You found a match!"); // display match found message
+			// move cards to matched cards array
+		}
+		else {
+			alert("Sorry, try again.");	// display match not found
+			// move cards back to cards[] array
+		}
+	}
+	else {
+		// nothing happens
+	}
+}
+
+selectCard();
+
+//ALERT MESSAGE PSEUDO CODE
+// notify winning/losing message
+// cards selected do not match
+// selected cards match
+// alert user with winning message
